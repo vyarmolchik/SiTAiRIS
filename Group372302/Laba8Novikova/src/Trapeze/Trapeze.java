a30f9ac3cf89e2f27658a9941e8a7a3e00449964
ksuhaN <ksu.novikova@gmail.com>
2015-12-22 16:32:29 +0300

    Lab8 JUnit Novikova

---
+++ Group372302/Laba8Novikova/src/Trapeze/Trapeze.java
@@ -0,0 +1,25 @@
+package Trapeze;
+
+import java.security.InvalidParameterException;
+
+public class Trapeze {
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int h;
+
+    public Trapeze(){}
+
+    public int perimeter(int a, int b, int c, int d){
+        if (a <= 0 || b <= 0 || c <= 0 || d <= 0)
+            throw new InvalidParameterException("Стороны должны быть положительными!");
+        return a + b + c + d;
+    }
+
+    public double square(int a, int b, int h){
+        if (a < 0 || b < 0 || h < 0)
+            throw new InvalidParameterException("Стороны должны быть положительными!");
+        return ((a + b) / 2.0) * h;
+    }
+}
